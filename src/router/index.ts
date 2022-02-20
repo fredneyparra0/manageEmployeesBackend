8024358e20a13679afb62a0c74ccc5d7bd64cd1c
fredneyparra0 <fredneyparra0@gmail.com>
2022-02-19 23:11:25 -0400

    routes configurate

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,23 @@
 import { Router } from 'express';
 
+import { 
+    getEmployees,
+    getEmployeeById,
+    createEmployee,
+    deleteEmployee,
+    updateEmployee
+} from '../controllers/employeesController';
+
+
+
 export const employeeRouter = Router();
 
-employeeRouter.get('',(req:any, res:any) => {
-    res.send('hello world')
-});
+employeeRouter.get('/employees', getEmployees);
+
+employeeRouter.get('/employee/:id', getEmployeeById);
+
+employeeRouter.post('/employee', createEmployee);
+
+employeeRouter.delete('/employee/:id', deleteEmployee);
+
+employeeRouter.put('/employee/:id', updateEmployee);
